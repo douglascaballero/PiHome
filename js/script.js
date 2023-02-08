66b12cb009fec5967dd5bc39ec4bf4bbb2785d3c
Douglas Caballero <douglascaballero16@gmail.com>
2023-02-08 19:39:49 +0400

    transicion left panel and new files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+function menudash(){
+    var p = 0;
+    var left_panel = document.getElementById("left-menu")
+    var text_home = document.getElementsByClassName("text-home");
+    if(left_panel.clientWidth == 40){
+        zoom_out();
+    }
+    else{
+        zoom_in();
+    }
+}
+
+function zoom_out(){
+    var left_panel = document.getElementById("left-menu");
+    var text_home = document.getElementById("text-home");
+    left_panel.style.cssText = "width: 300px";
+    text_home.style.cssText = "display: flex; margin-left: 50px;";
+}
+
+function zoom_in(){
+    var left_panel = document.getElementById("left-menu")
+    var text_home = document.getElementById("text-home");
+    left_panel.style.cssText = "width: 40px";
+    text_home.style.cssText = "display: none";
+}
+
 function prueba(){
     alert("Mas")
 }
+
